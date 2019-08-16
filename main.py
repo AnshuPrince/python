6b7a1238152eb746bc2b242722b1f051ac0dad17
Anshu Prince <princeanshu101.ap@gmail.com>
2019-08-17 03:45:02 +0530

    can view tabular data

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from tkinter import filedialog
+from tkinter import filedialog,ttk
 from tkinter import *
+from tkinter.ttk import Treeview
 
 mainWindow = Tk()
 
@@ -7,8 +8,24 @@ mainWindow = Tk()
 def browseAndReadFile():
     mainWindow.fileName = filedialog.askopenfilename(initialdir="/", title="Select file",filetypes=(("kget files", "*.log"), ("all files", "*.*")))
     file = open(mainWindow.fileName)
-
-
+    tree = ttk.Treeview(mainWindow)
+    tree["columns"] = ("EUtranCellFDD")
+    tree.column("EUtranCellFDD", width=400)
+    tree.heading("EUtranCellFDD", text="EUtranCellFDD")
+    ID = []
+    for line in file:
+        index = line.find('EUtranCellFDD=')
+        id = line[index: index+33]
+        if index > -1 and id not in ID:
+            ID.append(id)
+    for index, w in enumerate(ID):
+        tree.insert("", index+1, text="ID", values=(w))
+    # tree.insert("", 0, text="Line 1", values=("1A", "1b"))
+    # tree.insert("", 1, text="Dir 2", values=("2A", "2B"))
+    # ##alternatively:
+    # tree.insert("", 3, "dir3", text="Dir 3")
+    # tree.insert("dir3", 3, text=" sub dir 3", values=("3A", " 3B"))
+    tree.pack()
 
 menu = Menu(mainWindow)
 mainWindow.config(menu=menu)
